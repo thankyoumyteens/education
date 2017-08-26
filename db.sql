122124e85d476b7c20f74c07749d6df8bfb9d79d
ricky <iloveyesterday@outlook.com>
2017-08-26 15:16:39 +0800

    login

--- db.sql
+++ db.sql
@@ -5,13 +5,13 @@ create table t_user(
 	id int auto_increment primary key not null,
 	uid text not null,
 	pwd text
-);
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 create table t_exams(
   id int auto_increment primary key not null,
   title text,
   link text
-);
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
 create table t_course(
   id int auto_increment primary key not null,
@@ -19,16 +19,19 @@ create table t_course(
   title text,
   img text,
   link text
-);
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8;
 
-insert into t_exams(title, link) values('2015年中级经济师农业经济真题文字版', '2015_01.doc');
-insert into t_exams(title, link) values('2016年中级经济师农业经济真题文字版', '2016_01.doc');
-insert into t_exams(title, link) values('2017年中级经济师农业经济真题文字版', '2017_01.doc');
-insert into t_exams(title, link) values('2017年高级级经济师农业经济真题文字版', 'http://localhost:9999/exams/1.rar');
+insert into t_user(uid, pwd) values('test', 'test');
+
+insert into t_exams(title, link) values('2017年高级级经济师农业经济真题文字版', '/course/exams/1.rar');
 
 
 insert into t_course(category, title, img, link) values(0, '初级经济基础', 'http://img.mukewang.com/529dc3380001379906000338-240-135.jpg', 'static.hdslb.com/miniloader.swf?aid=13556227&page=1');
+insert into t_course(category, title, img, link) values(1, '中级经济基础', 'http://img.mukewang.com/529dc3380001379906000338-240-135.jpg', 'static.hdslb.com/miniloader.swf?aid=13556227&page=1');
+insert into t_course(category, title, img, link) values(2, '高级经济基础', 'http://img.mukewang.com/529dc3380001379906000338-240-135.jpg', 'static.hdslb.com/miniloader.swf?aid=13556227&page=1');
 insert into t_course(category, title, img, link) values(0, '初级人力资源', 'http://img.mukewang.com/57035ff200014b8a06000338-240-135.jpg', 'static.hdslb.com/miniloader.swf?aid=12056715&page=1');
+insert into t_course(category, title, img, link) values(1, '中级人力资源', 'http://img.mukewang.com/57035ff200014b8a06000338-240-135.jpg', 'static.hdslb.com/miniloader.swf?aid=12056715&page=1');
+insert into t_course(category, title, img, link) values(2, '高级人力资源', 'http://img.mukewang.com/57035ff200014b8a06000338-240-135.jpg', 'static.hdslb.com/miniloader.swf?aid=12056715&page=1');
 
 
 
--- web/index.jsp
+++ web/index.jsp
@@ -6,12 +6,17 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>优仕通</title>
-    <meta charset="UTF-8">
+<!DOCTYPE html><html><head><meta charset=utf-8><title>优仕通</title><style>* {
+margin: 0;
+padding: 0;
+}</style>
+<link rel=stylesheet href=static/css/bootstrap.css>
+<link href=static/css/app.cb40468d204cd1737aa2d1ceb487ad12.css rel=stylesheet>
 </head>
 <body>
-
+<div id=app></div>
+<script type=text/javascript src=static/js/manifest.6b670e3cdb35017eebb2.js></script>
+<script type=text/javascript src=static/js/vendor.3417fd9eca4e9cf6c036.js></script>
+<script type=text/javascript src=static/js/app.d9594160a641768c6ba9.js></script>
 </body>
 </html>
